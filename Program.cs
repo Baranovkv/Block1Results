595b807febe96b7bda6abbceca2aef1e0889d2b6
Kirill <k-baranov@outlook.com>
2023-02-13 20:45:46 +0300

    added cs file

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter array elements separated by commas");
+string [] array = Console.ReadLine().Split(", ");
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length < 6)
+        count ++;
+Console.WriteLine(count);
+Console.Write("[");
+if (count > 0)
+{
+    int j = 0;
+    string[] arrayResult = new string [count];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length < 6)
+        {
+            arrayResult[j] = array[i];
+            j++;
+        }
+    for (int i = 0; i < count-1; i++)
+        Console.Write($"\"{arrayResult[i]}\" ");
+    Console.Write($"\"{arrayResult[arrayResult.Length-1]}\"]");
+}    
+else 
+    Console.Write("]"); 
